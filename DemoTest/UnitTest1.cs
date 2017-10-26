9973f3cbd2b636b16b3f3c77112fe07c720a7014
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-10-26 10:27:16 +0200

    Initial commit

---
+++ DemoTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamCityTestProject;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd1()
+        {
+            var demo = new Demo();
+            var result = demo.Add(2, 2);
+            var expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestAdd2()
+        {
+            var demo = new Demo();
+            var result = demo.Add(0, 5);
+            var expectedResult = 5;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestMultiply1()
+        {
+            var demo = new Demo();
+            var result = demo.Multiply(2, 2);
+            var expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestMultiply2()
+        {
+            var demo = new Demo();
+            var result = demo.Multiply(5, 10);
+            var expectedResult = 50;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ TeamCityTestProject/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityTestProject
+{
+    public class Demo
+    {
+
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
